542808915a31688568420b40af91927f32f5da0d
Abhishek C <abhishek.chirayil.official@gmail.com>
2025-09-06 16:21:31 +0530

    skeleton

--- main.java
+++ main.java
@@ -1,10 +1,73 @@
-class Main{
-  public static void main(String[] args){
-    System.out.println("Hello World");
-    System.out.println("push1");
-    System.out.println("push2");
-    System.out.println("push3");
-    System.out.println("push4");
-    System.out.println("push5");
-  }
+// Donor class
+class Donor {
+    private String donorID;
+    private String name;
+    private int age;
+    private String bloodGroup;
+    private String contactInfo;
+    private boolean eligible;
+
+    public void register() {}
+    public void login() {}
+    public void acceptRequest() {}
+    public void cancelRequest() {}
+    public void viewProfile() {}
+    public void logDonation() {}
 }
+
+// Acceptor class
+class Acceptor {
+    private String acceptorID;
+    private String name;
+    private String contactInfo;
+
+    public void register() {}
+    public void login() {}
+    public void requestBlood() {}
+    public void viewStatus() {}
+}
+
+// Hospital class
+class Hospital {
+    private String hospitalID;
+    private String name;
+    private String location;
+
+    public void login() {}
+    public void conductBloodTest() {}
+    public void updateRecord() {}
+    public void viewRequests() {}
+}
+
+// Admin class
+class Admin {
+    private String adminID;
+    private String username;
+    private String password;
+
+    public void login() {}
+    public void moderateRequests() {}
+    public void handleIssues() {}
+}
+
+// BloodRequest class
+class BloodRequest {
+    private String requestID;
+    private String bloodGroup;
+    private int units;
+    private String status;
+
+    public void createRequest() {}
+    public void updateStatus() {}
+}
+
+// Notification class
+class Notification {
+    private String notificationID;
+    private String message;
+    private String timestamp;
+
+    public void send() {}
+    public void view() {}
+}
+
